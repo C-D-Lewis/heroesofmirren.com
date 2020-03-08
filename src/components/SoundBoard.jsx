b118aa30b3d79154e8a5a811aecebcb5168d9650
C-D-Lewis <Bonsitm@gmail.com>
2020-03-08 21:46:14 +0000

    Cache and play the sounds

--- src/components/SoundBoard.jsx
+++ src/components/SoundBoard.jsx
@@ -1,34 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Sounds } from '../assets';
 import Container from './Container.jsx'
 
-const SoundByte = ({ data }) =>
-  <Container
-    onClick={() => /* play {data.sound} */ {}}
-    style={{
-      backgroundColor: 'white',
-      borderRadius: 10,
-      maxWidth: 120,
-      margin: 5,
-    }}>
-    <img
-      src={`./assets/icons/${data.icon}`}
+const SoundByte = ({ data }) => {
+  const [audio, setAudio] = useState(null);
+
+  useEffect(() => {
+    const audioEl = new Audio(`./assets/sounds/${data.sound}`);
+    setAudio(audioEl);
+  }, []);
+
+  return (
+    <Container
+      onClick={() => audio.play()}
       style={{
-        width: '100%',
-        height: 'auto',
-        maxHeight: 70,
-      }} />
-    <span
-      style={{
-        fontSize: '0.8rem',
-        width: '100%',
-        display: 'flex',
-        justifyContent: 'center',
-        padding: 5,
+        backgroundColor: 'white',
+        borderRadius: 10,
+        maxWidth: 120,
+        margin: 5,
       }}>
-      {data.label}
-    </span>
-  </Container>;
+      <img
+        src={`./assets/icons/${data.icon}`}
+        style={{
+          width: '100%',
+          height: 'auto',
+          maxHeight: 70,
+        }} />
+      <span
+        style={{
+          fontSize: '0.8rem',
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+          padding: 5,
+        }}>
+        {data.label}
+      </span>
+    </Container>
+  );
+};
 
 const SoundBoard = () =>
   <Container
